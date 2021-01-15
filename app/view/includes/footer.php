f21e00a82864c3b158c7921ef4a6a99937308d48
Melquisedeque <melque.melque.ifg@hotmail.com>
2021-01-15 11:15:07 -0300

    UPDATE - inclusão de bootstrap online

--- app/view/includes/footer.php
+++ app/view/includes/footer.php
@@ -2,7 +2,7 @@
     <!--final div container body -->
     </div>
     <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="./js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
     <script script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
   </body>
 </html>
